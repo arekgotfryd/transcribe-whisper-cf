449b81eb627abe413d743aa5251c304625516a69
arekgotfryd <arekgotfryd@g.pl>
2025-01-14 15:30:38 +0100

    add filename and lagnuage query params

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,50 @@
-import { Context, Hono } from 'hono'
-import { Buffer } from 'buffer';
+import { Context, Hono } from "hono";
+import { Buffer } from "buffer";
+import { supportedLanguages } from "../supportedLanguages";
 export interface Env {
   AI: Ai;
 }
-const app = new Hono<{ Bindings: CloudflareBindings }>()
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/transcribe', async (c: Context) => {
-    // const res = await fetch(
-    //   "https://github.com/Azure-Samples/cognitive-services-speech-sdk/raw/master/samples/cpp/windows/console/samples/enrollment_audio_katie.wav"
-    // );
-    const res = await fetch(
-      "https://pub-4b41464ccabc4f95b1a65893433ef561.r2.dev/yt1z.net%20-%20Juliette%20Binoche%20and%20Ralph%20Fiennes%E2%80%99s%20Closet%20Picks.mp3"
-    );
-    const blob = await res.arrayBuffer();
+app.get("/transcribe", async (c: Context) => {
+  const filename = c.req.query("filename");
+  const lang = c.req.query("lang") || "en"; // Default to 'en' if lang is not provided
 
-    const input = {
-      audio: Buffer.from(blob).toString('base64'),
-    };
-    console.log("AI")
-    console.log(c.env)
-    console.log(c.env.AI)
-    const response = await c.env.AI.run(
-      "@cf/openai/whisper-large-v3-turbo",
-      input
-    );
+  if (!filename) {
+    return c.text("Filename query parameter is required", 400);
+  }
+  if (!lang || !supportedLanguages.includes(lang)) {
+    return c.text("Invalid language", 400);
+  }
 
-    return Response.json({ input: { audio: [] }, response });
-})
+  const res = await fetch(
+    `https://pub-4b41464ccabc4f95b1a65893433ef561.r2.dev/${filename}`
+  );
+  const blob = await res.arrayBuffer();
 
-app.get('/lama', async (c: Context) => {
-    const response = await c.env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
-      prompt: "What is the origin of the phrase Hello, World",
-    });
+  const input = {
+    audio: Buffer.from(blob).toString("base64"),
+    task: "transcribe",
+    language: lang,
+  };
+  const response = await c.env.AI.run(
+    "@cf/openai/whisper-large-v3-turbo",
+    input
+  );
 
-    return new Response(JSON.stringify(response));
-})
+  return Response.json({ input: { audio: [], lang: lang }, response });
+});
 
-export default app
+app.get("/lama", async (c: Context) => {
+  const response = await c.env.AI.run("@cf/meta/llama-3.1-8b-instruct", {
+    prompt: "What is the origin of the phrase Hello, World",
+  });
+
+  return new Response(JSON.stringify(response));
+});
+
+export default app;
